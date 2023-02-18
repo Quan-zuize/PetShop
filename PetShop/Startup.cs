eefc19126c9611676f66d3251f8a22ec24a61974
Reryuu <huy.nd.a1908g09@aptechlearning.edu.vn>
2023-02-18 20:30:15 +0700

    a

---
+++ PetShop/Startup.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace PetShop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddDistributedMemoryCache();           
+            services.AddSession(cfg => {                    
+                cfg.Cookie.Name = "cookie01";             
+                cfg.IdleTimeout = new TimeSpan(0, 60, 0);    
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env) 
+        {
+            app.UseSession();
+        }
+    }
+}
