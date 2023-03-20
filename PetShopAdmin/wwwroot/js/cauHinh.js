5aa9b0774f87cf23d63aae23467783c2f1072764
ngocthuy19 <quan.na.a1908g10@aptechlearning.edu.vn>
2023-03-20 20:56:13 +0700

    add cms config

---
+++ PetShopAdmin/wwwroot/js/cauHinh.js
@@ -0,0 +1,38 @@
+﻿R.CauHinh = {
+    Init: function () {
+        R.CauHinh.InitCkEditor();
+        R.CauHinh.RegisterEvent();
+    },
+    RegisterEvent: function () {
+        $('#edit-cauhinh').off('click').on('click', function () {
+            var tenCauHinh = $('#TenCauHinh').val();
+            var giaTriCauHinh = $('#GiaTriCauHinh').attr('src') ? $('#GiaTriCauHinh').attr('src') : CKEDITOR.instances.ckGiaTriCauHinh.getData();
+            var params = {
+                Id: 0,
+                TenCauHinh: tenCauHinh,
+                GiaTriCauhinh: giaTriCauHinh,
+            }
+            EditCauHinh(params);
+        })
+
+    },
+    InitCkEditor: function () {
+        CKEDITOR.replace('ckGiaTriCauHinh');
+        R.CauHinh.RegisterEvent();
+    },
+    CreateCauHinh: function (params) {
+        $.post('/CauHinhs/Create', params, function (response) {
+            alert('Them moi thanh cong!');
+            window.location.href = "/CauHinhs/Index"
+        })
+    },
+    EditCauHinh: function (params) {
+        $.post('/CauHinhs/Edit', params, function (response) {
+            alert('Chinh sua thanh cong!');
+            window.location.href = "/CauHinhs/Index"
+        })
+    }
+
+
+}
+R.CauHinh.Init();
---
+++ PetShopAdmin/wwwroot/js/uploadFile.js
@@ -0,0 +1,40 @@
+﻿R.UploadFile = {
+    Init: function () {
+        R.UploadFile.RegisterEvent();
+    },
+    RegisterEvent: function () {
+        $('.upload-single-input').off('change').on('change', function () {
+            var url = $('.upload-single-input')[0].files[0]
+            R.UploadFile.ShowImageAfterUpload($(this), url)
+
+            ClickButtonSubmit();
+        })
+    },
+    DoUpload: function (dt, element) {
+        $.ajax({
+            method: 'post',
+            url: '/UploadFiles/UploadSingleFile',
+            data: dt,
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                //var url = response.replace('\\', '/')
+                //R.UploadFile.ShowImageAfterUpload(element, url);
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        })
+    },
+    ShowImageAfterUpload: function (element, url) {
+        $(element).parent().find('.preview-picture').attr('src', url)
+    },
+    ClickButtonSubmit: function() {
+        $('.upload-single-btn').off('click').on('click', function () {
+            var dt = new FormData();
+            dt.append('file', $('.upload-single-input')[0].files[0]);
+            R.UploadFile.DoUpload(dt, $('.upload-single-input'));
+        })
+    }
+}
+R.UploadFile.Init();
